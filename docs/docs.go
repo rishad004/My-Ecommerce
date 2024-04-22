c89e844b2965bcbcc3890e9fde9375f429c43aee
rishad004 <rishadmuthu004@gmail.com>
2024-04-22 16:51:10 +0530

    set

--- docs/docs.go
+++ docs/docs.go
@@ -1506,7 +1506,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://byecom.shop",
+	Host:             "byecom.shop",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "B Y E C O M  LTD",
